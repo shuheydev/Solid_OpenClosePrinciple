6d2d3e661dda2016328a321de6de75b5b6ef6a31
shuheydev <shuhey.dev@gmail.com>
2020-08-07 19:22:22 +0900

    Usin Create method of AccountProcessor property in PersonModels instead of using Create method in Accounts's class directory.

--- Solid_OpenClosePrinciple/Program.cs
+++ Solid_OpenClosePrinciple/Program.cs
@@ -15,11 +15,10 @@ namespace Solid_OpenClosePrinciple
             };
 
             List<EmployeeModel> employees = new List<EmployeeModel>();
-            Accounts accountProcessor = new Accounts();
 
             foreach (var person in applicants)
             {
-                employees.Add(accountProcessor.Create(person));
+                employees.Add(person.AccountProcessor.Create(person));
             }
 
             foreach (var emp in employees)
