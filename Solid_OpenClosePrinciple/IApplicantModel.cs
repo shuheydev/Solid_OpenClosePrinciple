5e1b9cd0ec0c4d0ade15d6e0629dc2afd060ca49
shuheydev <shuhey.dev@gmail.com>
2020-08-07 19:17:20 +0900

    Added AccountProcessor property to IApplicantModel interface.

--- Solid_OpenClosePrinciple/IApplicantModel.cs
+++ Solid_OpenClosePrinciple/IApplicantModel.cs
@@ -4,5 +4,6 @@
     {
         string FirstName { get; set; }
         string LastName { get; set; }
+        IAccounts AccountProcessor { get; set; }
     }
 }
--- Solid_OpenClosePrinciple/PersonModel.cs
+++ Solid_OpenClosePrinciple/PersonModel.cs
@@ -4,5 +4,7 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public IAccounts AccountProcessor { get; set; } = new Accounts();
     }
 }
